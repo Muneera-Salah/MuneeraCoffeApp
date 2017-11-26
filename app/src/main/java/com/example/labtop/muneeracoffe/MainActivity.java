b11e2539154eca62d6e6b7ec60a5b2c7314f99a6
Muneera_salah <muneera.salah@gmail.com>
2017-11-27 02:30:54 +0300

    MuneeraCoffeApp

---
+++ app/src/main/java/com/example/labtop/muneeracoffe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.labtop.muneeracoffe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
